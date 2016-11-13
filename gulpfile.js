da43ee35b825c5590717366fad17bd24127e23e9
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-13 15:55:01 -0800

    update asset to assets

--- gulpfile.js
+++ gulpfile.js
@@ -50,13 +50,13 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy asset libraries from /node_modules into /asset
+// Copy assets libraries from /node_modules into /assets
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('asset/bootstrap'))
+        .pipe(gulp.dest('assets/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('asset/jquery'))
+        .pipe(gulp.dest('assets/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -66,7 +66,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('asset/font-awesome'))
+        .pipe(gulp.dest('assets/font-awesome'))
 })
 
 // Run everything
